c3d1cd5768bdc45a06059ebfd402a1e129d97c39
Undes1978 <avinash.dyagala@gmail.com>
2019-07-30 00:57:30 -0400

    updated sql connection

--- AddApplication.php
+++ AddApplication.php
@@ -70,13 +70,22 @@ function test_input($data) {
 
 function insert_db($Application_ID,$Application_Name,$Application_Type,$Application_Category,$Application_Prog_Language) 
 {
-	$servername = "localhost";
+	$servername = "dtemdm01.mysql.database.azure.com";
+	$username = "temdbmadm@dtemdm01";
+	$password = "waheguru@1112";
+	$options = array(
+    	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    	PDO::MYSQL_ATTR_SSL_CA => '/SSL/BaltimoreCyberTrustRoot.crt',
+    	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+	);
+
+/*	$servername = "localhost";
 	$username = "root";
 	$password = "temjul19";
-	$dbname = "dbtemd01";
+	$dbname = "dbtemd01";*/
     $result = "";
 	try {
-    	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	    $conn = new PDO("mysql:host=$servername;port=3306;dbname=dtemdb01", $username, $password, $options);
     	// set the PDO error mode to exception
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     	$sql = "INSERT INTO application (Application_ID,Application_Name,Application_Type,Application_Category,Application_Prog_Language)
